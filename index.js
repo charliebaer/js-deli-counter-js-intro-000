fcaf8b3eba4dd9bdc4ce76f39c6d5abdc08f6d5a
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 12:27:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line,name){
   line.push(name);
-  console.log(`Welcome ${name}.You are ${line.length} in line.`)
+  return(`Welcome ${name}.You are ${line.length} in line.`)
 }
