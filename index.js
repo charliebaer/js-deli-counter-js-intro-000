db491ecbf0c3485fedefd202b3ef2f68582e7fa3
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-18 12:50:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ function takeANumber(line,name){
 }
 function nowServing(line){
   if(line.length > 0){
-    return("Currently serving "+line.shift()+".")
+    console.log("Currently serving "+line.shift()+".")
   }
   else return("There is nobody waiting to be served!")
+}
+currentLine(line){
+  if(line.length > 0){
+    retu
+  }
 }
